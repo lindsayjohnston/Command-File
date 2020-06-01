c6ccb3c9c06c4da1763b445ede3e68a92485823a
Lindsay Johnston <lindsaykjohnston@outlook.com>
2020-06-01 13:08:17 -0700

    Added Clear Command function and Filter Command function

--- docs/app.js
+++ docs/app.js
@@ -3,6 +3,8 @@
 const form= document.querySelector('#command-form');
 const commandList= document.querySelector('.collection');
 const commandInput= document.querySelector('#command');
+const clearBtn= document.querySelector('.clear-commands');
+const filter=document.querySelector('#filter');
 
 //LOAD EVENT LISTENERS
 
@@ -11,6 +13,8 @@ loadEventListeners();
 function loadEventListeners(){
     form.addEventListener('submit', addTask);
     commandList.addEventListener('click', removeTask);
+    clearBtn.addEventListener('click', clearCommands);
+    filter.addEventListener('keyup',filterCommands );
 }
 
 function addTask(event){
@@ -39,4 +43,24 @@ function removeTask(event){
             event.target.parentElement.parentElement.remove();
         }
     };
+}
+
+function clearCommands(){
+    if(confirm("Are you sure?")){
+        while(commandList.firstChild){
+            commandList.removeChild(commandList.firstChild);
+        }
+    }
+}
+
+function filterCommands(event){
+    const text= (event.target.value.toLowerCase());
+    document.querySelectorAll('.collection-item').forEach(function(command){
+        const item= (command.firstChild.textContent.toLowerCase());
+        if(item.indexOf(text) !== -1){
+            command.style.display='block';
+        } else{
+            command.style.display='none';
+        }
+    })
 }
